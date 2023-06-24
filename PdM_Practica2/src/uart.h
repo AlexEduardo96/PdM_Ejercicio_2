6dce972fbd8ec6341cc32823150feba0a111c8a5
Paul180298 <paul.jimeneza@ucuenca.edu.ec>
2023-06-23 23:46:52 -0500

    Comentarios
    Agrego comentarios de estado de los pulsantes

--- PdM_Practica2/src/uart.h
+++ PdM_Practica2/src/uart.h
@@ -4,11 +4,11 @@
 void myPrint(dbn_t* data){
   if (data->estado == 1){
     Serial.print("Tecla: ");
-    Serial.print(data->tecla-1);
+    Serial.print(data->tecla-1); //Print in the UART communication of the state of the Key
     Serial.print("\t Estado: Presionado\n");
   } else if (data->estado == 0){
     Serial.print("Tecla: ");
-    Serial.print(data->tecla-1);
+    Serial.print(data->tecla-1); //Print in the UART communication of the state of the Key
     Serial.print("\t Estado: Liberado\n");
   }
   
